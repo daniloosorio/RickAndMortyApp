41773f2f3507d47e524affce5efa3b840fa49f9b
Danilo Osorio <odanilo38@gmail.com>
2025-01-27 21:25:56 -0500

    Initial Commit

---
+++ RickAndMortyApp/RickAndMortyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyAppApp.swift
+//  RickAndMortyApp
+//
+//  Created by Danilo Osorio on 27/01/25.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
